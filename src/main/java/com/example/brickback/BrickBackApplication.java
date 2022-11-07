90ce90b1fddab389a0cec2376457d937158fc42a
Seungyeup <lsyeup1206@naver.com>
2022-11-07 23:13:36 +0900

    brick back start

---
+++ src/main/java/com/example/brickback/BrickBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.brickback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrickBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brickback/BrickBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brickback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
