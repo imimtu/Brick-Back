cb94856bd0774445938cafe70aacade3079a39c3
Seungyeup <lsyeup1206@naver.com>
2022-12-29 17:13:54 +0900

    test

--- src/main/java/com/im2/brickback/presentation/SampleController.java
+++ src/main/java/com/im2/brickback/presentation/SampleController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     @GetMapping("/test")
-    public String sampleTest(){
-        return "sampleTest";
-    }
+    public String sampleTest(){ return "sampleTest"; }
 
 }
