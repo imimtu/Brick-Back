034bdd984f520c7990b821d034a7ab60285204eb
Seungyeup <lsyeup1206@naver.com>
2023-01-03 22:51:14 +0900

    #5 - git kraken 연동 정상
    git branch 관리 -> git kraken

--- src/main/java/com/im2/brickback/controller/SampleController.java
+++ src/main/java/com/im2/brickback/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.im2.brickback.presentation;
+package com.im2.brickback.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
