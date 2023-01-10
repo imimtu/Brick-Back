e1bc61ec6198fb87e7e76598adbb14e16834d0b1
Seungyeup <lsyeup1206@naver.com>
2023-01-11 00:06:55 +0900

    SQL Dummy & Test data

--- src/test/java/com/im2/brickback/repository/JPARepositoryTest.java
+++ src/test/java/com/im2/brickback/repository/JPARepositoryTest.java
@@ -1,11 +1,16 @@
 package com.im2.brickback.repository;
 
 import com.im2.brickback.config.JpaConfig;
+import com.im2.brickback.domain.Brick;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
@@ -15,8 +20,27 @@ class JPARepositoryTest {
     private final BrickRepository brickRepository;
     private final UserAccountRepository userAccountRepository;
 
-    public JPARepositoryTest(BrickRepository brickRepository, UserAccountRepository userAccountRepository) {
+    public JPARepositoryTest(
+            @Autowired BrickRepository brickRepository,
+            @Autowired UserAccountRepository userAccountRepository
+    ) {
         this.brickRepository = brickRepository;
         this.userAccountRepository = userAccountRepository;
     }
+
+    @DisplayName("Select Test")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+
+        // Given
+
+        // When & Then
+        List<Brick> bricks = brickRepository.findAll();
+
+        assertThat(bricks)
+                .isNotNull()
+                .hasSize(0);
+
+    }
+
 }
