45e8f44e002a67ebd787fb5aa509c30695ec4f0e
Seungyeup <lsyeup1206@naver.com>
2023-03-01 22:57:01 +0900

    login function pre

---
+++ src/main/java/com/im2/brickback/controller/request/UserLoginRequest.java
@@ -0,0 +1,9 @@
+package com.im2.brickback.controller.request;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UserLoginRequest {
+    private String userName;
+    private String userPassword;
+}
--- src/main/java/com/im2/brickback/service/UserService.java
+++ src/main/java/com/im2/brickback/service/UserService.java
@@ -7,6 +7,7 @@ import com.im2.brickback.exception.ErrorCode;
 import com.im2.brickback.repository.BrickRepository;
 import com.im2.brickback.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
+    @Value("this_is_example_secret_key.for_brick_users_authorization")
+    private String secretKey;
+
+    @Value("2592000000")
+    private String expiredTimeMs;
+
     private final UserEntityRepository userEntityRepository;
     private final BCryptPasswordEncoder encoder;
 
@@ -22,11 +29,28 @@ public class UserService {
     public User join(String userId, String userPassword){
         // 유저명 중복인지?
         userEntityRepository.findByUserId(userId).ifPresent(it -> {
-            throw new BrickApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userId ));
+            throw new BrickApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated.", userId ));
         });
         // 회원가입 진행
         UserEntity userEntity = userEntityRepository.save(UserEntity.of(userId, encoder.encode(userPassword)));
         return User.fromEntity(userEntity);
+    }
+
+    @Transactional
+    public String login(String userName, String userPassword) {
+        // 가입된 유저인지?
+        UserEntity userEntity = userEntityRepository.findByUserId(userName).orElseThrow(
+                () -> new BrickApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s is not found.", userName))
+        );
+
+        // 비밀번호 확인
+        if(!encoder.matches(userPassword, userEntity.getUserPassword())){
+            throw new BrickApplicationException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        // 토큰 생성 후 반환
+        String authToken = JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
+        return authToken;
     }
 
 }
